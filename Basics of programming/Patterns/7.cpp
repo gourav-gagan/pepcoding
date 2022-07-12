35f959156abe12087fcc112fe39cffad1f8b9040
gourav-gagan <gourav.gagan344@gmail.com>
2022-07-13 02:57:12 +0530

    pattern

---
+++ Basics of programming/Patterns/7.cpp
@@ -0,0 +1,28 @@
+//n=5                       front space
+// *	                     0
+//     *	                 1
+//         *	             2
+//             *	         3
+//                 *         4
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+
+    for (int i=1; i<=n; i++)
+    {
+        for (int j=1; j<=n; j++)
+        {
+            if (i==j)
+                cout<<"*\t";
+            else
+                cout<<"\t";
+        }
+        cout<<endl;
+    }
+}
