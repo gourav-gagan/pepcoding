302c5e088f9a509987717e56131cba6519be6b9e
gourav-gagan <gourav.gagan344@gmail.com>
2022-07-13 04:43:06 +0530

    patterns

--- Basics of programming/Patterns/12.cpp
+++ Basics of programming/Patterns/12.cpp
@@ -7,32 +7,22 @@
 #include <iostream>
 using namespace std;
 
-int fibonacci(int n)
-{
-    int nums[n];
-    nums[0]=0;
-    nums[1]=1;
-
-    for (int i=2; i<=n; i++)
-        nums[i] = nums[i-1] + nums[i-2];
-
-    int output = nums[n-1];
-    return output;
-}
-
 int main()
 {
     int n;
     cin>>n;
 
-    int count=0;
+    int a = 0;
+    int b = 1;
 
     for (int i=1; i<=n; i++)
     {
         for (int j=1; j<=i; j++)
         {
-            count++;
-            cout<<fibonacci(count)<<"\t";
+            cout<<a<<"\t";
+            int c = a+b;
+            a = b;
+            b = c;
         }
         cout<<endl;
     }
