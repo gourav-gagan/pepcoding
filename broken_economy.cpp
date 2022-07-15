4ca6bf9a92515dc7699cbe587687963119d42839
gourav-gagan <gourav.gagan344@gmail.com>
2022-07-16 03:27:49 +0530

    broken economy question

---
+++ broken_economy.cpp
@@ -0,0 +1,30 @@
+//https://www.pepcoding.com/resources/online-java-foundation/function-and-arrays/ceil-floor-official/ojquestion
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    int* arr = new int[n];
+    for(int i = 0 ; i < n; i++){
+        cin>>arr[i];
+    }
+    int data;
+    cin>>data;
+    
+    for (int i=0; i<n; i++)
+    {
+        if (arr[i]==data)
+        {
+            cout<<data;
+            break;
+        }
+        if (arr[i]>data)
+        {
+            cout<<arr[i]<<endl<<arr[i-1];
+            break;
+        }
+    }
+    
+}
