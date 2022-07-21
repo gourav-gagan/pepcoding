1878ff3205b2e234b4772877330fd6ce534a8682
gourav-gagan <gourav.gagan344@gmail.com>
2022-07-22 00:34:51 +0530

    get keypad combination

---
+++ Recursion and Backtracking/3. Recursion on vectors/get_key_pad_combinations.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+#include<string>
+#include<vector>
+using namespace std;
+
+vector<string> getKPC(string s) {
+
+    //define codes vector
+    static vector<string> codes = {".;","abc","def","ghi","jkl","mno","pqrs","tu","vwx","yz"};
+
+    //base case
+    if (s.length()==0){
+        static vector<string> base = {""}; //base case initialisation
+        return base;
+    }
+
+    //split
+    char s1 = s[0];
+    string s2 = s.substr(1);
+
+    //call recursion
+    vector<string> rec;
+    rec = getKPC(s2);
+
+    //final result
+    vector<string> result;
+
+    string codesfors1 = codes[s1-'0'];
+
+    for (int i=0; i<codesfors1.length(); i++)
+    {
+        char s1code = codesfors1[i];
+        for (auto el : rec){
+            result.push_back(s1code+el);
+        }
+    }
+
+    return result;
+}
+
+
+int main() {
+  string s;
+  cin >> s;
+  vector<string> ans = getKPC(s);
+  int cnt = 0;
+
+  cout << '[';
+  for (string str : ans) {
+    if (cnt != ans.size() - 1)
+      cout << str << ", ";
+    else
+      cout << str;
+    cnt++;
+  }
+  cout << ']';
+}
